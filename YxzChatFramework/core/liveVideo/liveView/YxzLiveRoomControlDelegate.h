f2bf8839e2f579f91aa538eab83fa285b7d3dcf6
颜学宙 <>
2020-07-30 14:42:51 +0800

    add

---
+++ YxzChatFramework/core/liveVideo/liveView/YxzLiveRoomControlDelegate.h
@@ -0,0 +1,18 @@
+//
+//  YxzLiveRoomControlDelegate.h
+//  suspensionPay
+//
+//  Created by 颜学宙 on 2020/7/30.
+//  Copyright © 2020 颜学宙. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol YxzLiveRoomControlDelegate <NSObject>
+-(void)suspensionClick;
+-(void)moreInfoClick;
+@end
+
+NS_ASSUME_NONNULL_END
